0626f396885d6a9529f55ffb267eaea291526cdf
Donnacha Heffernan <Donnacha.Heffernan@bd.com>
2022-04-24 14:11:03 +0100

    force redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'users', component: UsersListComponent }
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'users', component: UsersListComponent },
 ];
 
 @NgModule({
