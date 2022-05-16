3efc07f65042407ce370a73590ca5fa8bc790db5
Donnacha Heffernan <Donnacha.Heffernan@bd.com>
2022-05-16 09:26:57 +0100

    sort tests

--- src/app/users/users-list.component.spec.ts
+++ src/app/users/users-list.component.spec.ts
@@ -160,6 +160,58 @@ describe('UsersListComponent', () => {
             expect(component.dataSource).toEqual(expectedResult);
         });
 
+        it('should not sort if not active', () => {
+            // Arrange
+            const expectedResult = {data: [
+                    {name: 'John'},
+                    {name: 'Duke'}, 
+                    {name: 'Dave'}, 
+                    {name: 'Jude'},
+            ]};
+            const sort = {
+                active: null,
+                direction: 'asc'
+            } as Sort;
+            component.dataSource = {data: [
+                    {name: 'John'},
+                    {name: 'Duke'}, 
+                    {name: 'Dave'}, 
+                    {name: 'Jude'},
+            ]} as any
+
+            // Act
+            component.sortData(sort);
+
+            // Assert
+            expect(component.dataSource).toEqual(expectedResult);
+        });
+
+        it('should not sort if direction is empty', () => {
+            // Arrange
+            const expectedResult = {data: [
+                    {name: 'John'},
+                    {name: 'Duke'}, 
+                    {name: 'Dave'}, 
+                    {name: 'Jude'},
+            ]};
+            const sort = {
+                active: 'name',
+                direction: ''
+            } as Sort;
+            component.dataSource = {data: [
+                    {name: 'John'},
+                    {name: 'Duke'}, 
+                    {name: 'Dave'}, 
+                    {name: 'Jude'},
+            ]} as any
+
+            // Act
+            component.sortData(sort);
+
+            // Assert
+            expect(component.dataSource).toEqual(expectedResult);
+        });
+
         it('should sort by height in descending order', () => {
             // Arrange
             const expectedResult = {data: [
@@ -177,6 +229,240 @@ describe('UsersListComponent', () => {
                     {name: 'Duke', height: '14'}, 
                     {name: 'Dave', height: '1'}, 
                     {name: 'Jude', height: '32'},
+            ]} as any
+
+            // Act
+            component.sortData(sort);
+
+            // Assert
+            expect(component.dataSource).toEqual(expectedResult);
+        });
+
+        it('should sort by height in ascending order', () => {
+            // Arrange
+            const expectedResult = {data: [
+                    {name: 'Dave', height: '1'}, 
+                    {name: 'Duke', height: '14'}, 
+                    {name: 'John', height: '28'},
+                    {name: 'Jude', height: '32'},
+            ]};
+            const sort = {
+                active: 'height',
+                direction: 'asc'
+            } as Sort;
+            component.dataSource = {data: [
+                    {name: 'John', height: '28'},
+                    {name: 'Duke', height: '14'}, 
+                    {name: 'Dave', height: '1'}, 
+                    {name: 'Jude', height: '32'},
+            ]} as any
+
+            // Act
+            component.sortData(sort);
+
+            // Assert
+            expect(component.dataSource).toEqual(expectedResult);
+        });
+
+        it('should sort by mass in ascending order', () => {
+            // Arrange
+            const expectedResult = {data: [
+                    {name: 'Dave', mass: '10'}, 
+                    {name: 'Duke', mass: '140'}, 
+                    {name: 'John', mass: '280'},
+                    {name: 'Jude', mass: '3200'},
+            ]};
+            const sort = {
+                active: 'mass',
+                direction: 'asc'
+            } as Sort;
+            component.dataSource = {data: [
+                    {name: 'John', mass: '280'},
+                    {name: 'Jude', mass: '3200'},
+                    {name: 'Duke', mass: '140'}, 
+                    {name: 'Dave', mass: '10'}, 
+            ]} as any
+
+            // Act
+            component.sortData(sort);
+
+            // Assert
+            expect(component.dataSource).toEqual(expectedResult);
+        });
+
+        it('should sort by mass in descending order', () => {
+            // Arrange
+            const expectedResult = {data: [
+                    {name: 'Jude', mass: '3200'},
+                    {name: 'John', mass: '280'},
+                    {name: 'Duke', mass: '140'}, 
+                    {name: 'Dave', mass: '10'}, 
+            ]};
+            const sort = {
+                active: 'mass',
+                direction: 'desc'
+            } as Sort;
+            component.dataSource = {data: [
+                    {name: 'John', mass: '280'},
+                    {name: 'Jude', mass: '3200'},
+                    {name: 'Duke', mass: '140'}, 
+                    {name: 'Dave', mass: '10'}, 
+            ]} as any
+
+            // Act
+            component.sortData(sort);
+
+            // Assert
+            expect(component.dataSource).toEqual(expectedResult);
+        });
+
+        it('should sort by created in descending order', () => {
+            // Arrange
+            const expectedResult = {data: [
+                    {name: 'Dave', created: '2017-12-09T13:50:51.644000Z'}, 
+                    {name: 'Jude', created: '2014-12-09T13:50:51.644000Z'},
+                    {name: 'Duke', created: '2014-12-08T13:50:51.644000Z'}, 
+                    {name: 'John', created: '2014-11-09T13:50:51.644000Z'},
+            ]};
+            const sort = {
+                active: 'created',
+                direction: 'desc'
+            } as Sort;
+            component.dataSource = {data: [
+                    {name: 'John', created: '2014-11-09T13:50:51.644000Z'},
+                    {name: 'Jude', created: '2014-12-09T13:50:51.644000Z'},
+                    {name: 'Duke', created: '2014-12-08T13:50:51.644000Z'}, 
+                    {name: 'Dave', created: '2017-12-09T13:50:51.644000Z'}, 
+            ]} as any
+
+            // Act
+            component.sortData(sort);
+
+            // Assert
+            expect(component.dataSource).toEqual(expectedResult);
+        });
+
+        it('should sort by created in ascending order', () => {
+            // Arrange
+            const expectedResult = {data: [
+                    {name: 'John', created: '2014-11-09T13:50:51.644000Z'},
+                    {name: 'Duke', created: '2014-12-08T13:50:51.644000Z'}, 
+                    {name: 'Jude', created: '2014-12-09T13:50:51.644000Z'},
+                    {name: 'Dave', created: '2017-12-09T13:50:51.644000Z'}, 
+            ]};
+            const sort = {
+                active: 'created',
+                direction: 'asc'
+            } as Sort;
+            component.dataSource = {data: [
+                    {name: 'John', created: '2014-11-09T13:50:51.644000Z'},
+                    {name: 'Jude', created: '2014-12-09T13:50:51.644000Z'},
+                    {name: 'Duke', created: '2014-12-08T13:50:51.644000Z'}, 
+                    {name: 'Dave', created: '2017-12-09T13:50:51.644000Z'}, 
+            ]} as any
+
+            // Act
+            component.sortData(sort);
+
+            // Assert
+            expect(component.dataSource).toEqual(expectedResult);
+        });
+
+        it('should sort by edited in descending order', () => {
+            // Arrange
+            const expectedResult = {data: [
+                    {name: 'Dave', edited: '2017-12-09T13:50:51.644000Z'}, 
+                    {name: 'Jude', edited: '2014-12-09T13:50:51.644000Z'},
+                    {name: 'Duke', edited: '2014-12-08T13:50:51.644000Z'}, 
+                    {name: 'John', edited: '2014-11-09T13:50:51.644000Z'},
+            ]};
+            const sort = {
+                active: 'edited',
+                direction: 'desc'
+            } as Sort;
+            component.dataSource = {data: [
+                    {name: 'John', edited: '2014-11-09T13:50:51.644000Z'},
+                    {name: 'Jude', edited: '2014-12-09T13:50:51.644000Z'},
+                    {name: 'Duke', edited: '2014-12-08T13:50:51.644000Z'}, 
+                    {name: 'Dave', edited: '2017-12-09T13:50:51.644000Z'}, 
+            ]} as any
+
+            // Act
+            component.sortData(sort);
+
+            // Assert
+            expect(component.dataSource).toEqual(expectedResult);
+        });
+
+        it('should sort by edited in ascending order', () => {
+            // Arrange
+            const expectedResult = {data: [
+                    {name: 'Duke', edited: '2014-12-08T13:50:51.644000Z'}, 
+                    {name: 'John', edited: '2014-12-09T13:50:31.644000Z'},
+                    {name: 'Jude', edited: '2014-12-09T13:50:51.644000Z'},
+                    {name: 'Dave', edited: '2017-12-09T13:50:51.644000Z'}, 
+            ]};
+            const sort = {
+                active: 'edited',
+                direction: 'asc'
+            } as Sort;
+            component.dataSource = {data: [
+                    {name: 'John', edited: '2014-12-09T13:50:31.644000Z'},
+                    {name: 'Jude', edited: '2014-12-09T13:50:51.644000Z'},
+                    {name: 'Duke', edited: '2014-12-08T13:50:51.644000Z'}, 
+                    {name: 'Dave', edited: '2017-12-09T13:50:51.644000Z'}, 
+            ]} as any
+
+            // Act
+            component.sortData(sort);
+
+            // Assert
+            expect(component.dataSource).toEqual(expectedResult);
+        });
+
+        it('should sort homeworldName by ascending order', () => {
+            // Arrange
+            const expectedResult = {data: [
+                    {homeworldName: 'Earth'}, 
+                    {homeworldName: 'Jupiter'},
+                    {homeworldName: 'Mars'}, 
+                    {homeworldName: 'Venus'},
+            ]};
+            const sort = {
+                active: 'homeworldName',
+                direction: 'asc'
+            } as Sort;
+            component.dataSource = {data: [
+                    {homeworldName: 'Jupiter'},
+                    {homeworldName: 'Venus'}, 
+                    {homeworldName: 'Earth'}, 
+                    {homeworldName: 'Mars'},
+            ]} as any
+
+            // Act
+            component.sortData(sort);
+
+            // Assert
+            expect(component.dataSource).toEqual(expectedResult);
+        });
+
+        it('should sort homeworldName by descending order', () => {
+            // Arrange
+            const expectedResult = {data: [
+                    {homeworldName: 'Venus'},
+                    {homeworldName: 'Mars'},
+                    {homeworldName: 'Jupiter'}, 
+                    {homeworldName: 'Earth'}, 
+            ]};
+            const sort = {
+                active: 'homeworldName',
+                direction: 'desc'
+            } as Sort;
+            component.dataSource = {data: [
+                    {homeworldName: 'Jupiter'},
+                    {homeworldName: 'Earth'}, 
+                    {homeworldName: 'Mars'},
+                    {homeworldName: 'Venus'}, 
             ]} as any
 
             // Act
